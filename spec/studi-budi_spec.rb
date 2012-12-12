b3cf56e0a740d5b8861eeb023cecfbd9ea5e53c9
Sol W <capnsol@gmail.com>
2012-12-11 16:09:19 -0800

    documentation done-ish, working on tests

--- spec/studi-budi_spec.rb
+++ spec/studi-budi_spec.rb
@@ -1,13 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "StudiBudi" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  # it "fails" do
+  #   fail "hey buddy, you should probably rename this file and start specing for real"
+  # end
+  before do
+    @study_helper = StudiBudi.new
   end
 
+  it "should get the user's input" do
+    @study_helper.stub!(:gets) {"phrase"} # @study_helper.action.should eq 
+  end
 
 
   # "if I make a card collection called 'Potatos'" do
   #   adfld.@collection_name should_eq "Potatos"
   # end
+
+
+
+
+
+
+
+
+
 end
